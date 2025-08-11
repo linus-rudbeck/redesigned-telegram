58985bd27b59ed8a40811089b1fc8cb8753c0a04
linus-rudbeck <linus.rudbeck@distansakademin.se>
2025-08-11 14:06:13 +0200

    Initial code

---
+++ src/test/java/com/example/redesigned_telegram/RedesignedTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redesigned_telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedesignedTelegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
